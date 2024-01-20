8eaea3652b682d4e78baf799ad60c970c786185f
sneha931 <153498006+sneha931@users.noreply.github.com>
2024-01-20 21:59:24 +0530

    Add files via upload

---
+++ landingpage/land.js
@@ -0,0 +1,6 @@
+const menutoggle = document.querySelector('.toggle');
+const nav = document.querySelector('.nav');
+menutoggle.onclick=function(){
+    menutoggle.classList.toggle("active");
+    nav.classList.toggle("active");
+}
